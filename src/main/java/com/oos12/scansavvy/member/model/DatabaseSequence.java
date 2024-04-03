595dd7fe65531da2d7d5a348087bd52705350d38
Monggu-Park <tomas407@naver.com>
2024-04-03 17:27:27 +0900

    test

--- src/main/java/com/oos12/scansavvy/member/model/DatabaseSequence.java
+++ src/main/java/com/oos12/scansavvy/member/model/DatabaseSequence.java
@@ -8,9 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Setter
 @Document(collection = "database_sequences")
 public class DatabaseSequence {
-
     @Id
     private String id;
     private long seq;
-
 }
