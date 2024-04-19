50ca7291c6fb68a4c24e2443fb84d23c8888eb5f
Monggu-Park <tomas407@naver.com>
2024-04-19 14:37:20 +0900

    4.19

--- src/main/java/com/oos12/scansavvy/health/controller/EeveRestController.java
+++ src/main/java/com/oos12/scansavvy/health/controller/EeveRestController.java
@@ -5,10 +5,7 @@ import com.oos12.scansavvy.health.service.EeveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EeveRestController {
@@ -21,7 +18,7 @@ public class EeveRestController {
 
     @GetMapping("api/v1/ai/generate")
     public ResponseEntity<EeveResponse> generate(
-            @RequestParam(value = "promptMessage", defaultValue = "안녕하세요")
+            @RequestParam(value = "promptMessage", defaultValue = "hello")
             String promptMessage){
         final EeveResponse aiResponse = eeveService.generateMessage(promptMessage);
         return ResponseEntity.status(HttpStatus.OK).body(aiResponse);
