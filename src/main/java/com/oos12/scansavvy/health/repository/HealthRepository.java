e76c736f5ebadeda6aa5fe5b15ad57bc2c6aedd1
Mongu <130650162+Monggu-Park@users.noreply.github.com>
2024-06-08 00:10:02 +0900

    Delete src/main/java/com/oos12/scansavvy directory

--- src/main/java/com/oos12/scansavvy/health/repository/HealthRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oos12.scansavvy.health.repository;
-
-import com.oos12.scansavvy.health.model.Health;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface HealthRepository extends MongoRepository<Health, String> {
-}
--- src/main/java/com/oos12/scansavvy/health/service/EeveService.java
+++
@@ -1,8 +0,0 @@
-package com.oos12.scansavvy.health.service;
-
-import com.oos12.scansavvy.health.model.EeveResponse;
-
-public interface EeveService {
-    EeveResponse generateMessage(String prompt);
-    EeveResponse generateJoke(String topic);
-}
